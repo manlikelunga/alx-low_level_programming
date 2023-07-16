707b78702bd05b716db2eca16b414f100b8b24f6
manlikelunga <manlikelunga@gmail.com>
2023-07-16 11:43:53 -0700

    what is whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is main function
+ * main - this is the main function
  *
- * return: always returning 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
