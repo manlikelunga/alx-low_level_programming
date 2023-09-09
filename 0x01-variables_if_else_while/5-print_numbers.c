a19b0fb57e16d21a7255b637a4c696f323e6fd31
manlikelunga <manlikelunga@gmail.com>
2023-09-09 04:01:00 -0700

    this was easy

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit NOs base 10 from 0 followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
